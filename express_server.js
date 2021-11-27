7f41cd7b538d7240004b357277939db5ebd5e31c
Brian Duong <brianminhduong@gmail.com>
2021-11-27 16:25:51 +0000

    Require body-parser to read Buffer from POST request

--- express_server.js
+++ express_server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = 8080;
 
@@ -8,16 +9,19 @@ const urlDatabase = {
 };
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
 app.get("/", (req, res) => res.send("Hello!"));
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+app.get("/urls/new", (req, res) => res.render("urls_new"));
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
 app.get("/urls.json", (req, res) => res.json(urlDatabase));
 app.get("/hello", (req, res) => res.send("<html><body>Hello <b>World</b></body></html>\n"));
+
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
