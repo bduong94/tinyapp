5af1e306aa0562a5af1d448c5a82f87b6c9f218f
Brian Duong <brianminhduong@gmail.com>
2021-11-27 17:05:53 +0000

    Implement generateRandomString function in express_server.js

--- express_server.js
+++ express_server.js
@@ -2,11 +2,27 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const PORT = 8080;
+let urlLength = 6;
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+function generateRandomString() {
+  let result = "";
+  let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+  
+  for (let i = 0; i < urlLength; i++) {
+    result += characters[Math.floor(characters.length * Math.random())];
+  }
+
+  if (urlDatabase[result]) {
+    generateRandomString();
+  }
+
+  return result;
+}
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
@@ -16,6 +32,10 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 app.get("/urls/new", (req, res) => res.render("urls_new"));
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
+});
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
