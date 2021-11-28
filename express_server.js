f2fefe10db6a9cc71879c4a66c2f5d9bd9e6c143
Brian Duong <brianminhduong@gmail.com>
2021-11-28 01:25:12 +0000

    Make comments for express_server.js

--- express_server.js
+++ express_server.js
@@ -9,6 +9,7 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+//Create shortURL string
 function generateRandomString() {
   let result = "";
   let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
@@ -27,25 +28,35 @@ function generateRandomString() {
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.get("/", (req, res) => res.send("Hello!"));
+
+//Shows all URL's in the database
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+
+//Shows form to create new URL
 app.get("/urls/new", (req, res) => res.render("urls_new"));
+
+//Creates new shortURL and adds to database
 app.post("/urls", (req, res) => {
   let randomString = generateRandomString();
   urlDatabase[randomString] = req.body['longURL'];
   res.redirect(`/urls/${randomString}`);
 });
+
+//Shows all short URLS
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
+
+//Redirect to longURL
 app.get("/u/:shortURL", (req, res) => {
   res.redirect(urlDatabase[req.params.shortURL]);
 });
+
 app.get("/urls.json", (req, res) => res.json(urlDatabase));
 app.get("/hello", (req, res) => res.send("<html><body>Hello <b>World</b></body></html>\n"));
-
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
